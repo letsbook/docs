508ee68f8cef7674fe5f6772a10e6dcaebcda1b4
Laurent van Dommelen <laurent@lets-book.com>
2025-09-30 19:56:59 +0200

    Prettier on swizzled files

--- src/theme/BlogSidebar/index.tsx
+++ src/theme/BlogSidebar/index.tsx
@@ -1,17 +1,17 @@
-import React, {type ReactNode} from 'react';
-import {useWindowSize} from '@docusaurus/theme-common';
+import { useWindowSize } from '@docusaurus/theme-common';
+import type { Props } from '@theme/BlogSidebar';
 import BlogSidebarDesktop from '@theme/BlogSidebar/Desktop';
 import BlogSidebarMobile from '@theme/BlogSidebar/Mobile';
-import type {Props} from '@theme/BlogSidebar';
+import React, { type ReactNode } from 'react';
 
-export default function BlogSidebar({sidebar}: Props): ReactNode {
-  const windowSize = useWindowSize();
-  if (!sidebar?.items.length) {
-    return null;
-  }
-  // Mobile sidebar doesn't need to be server-rendered
-  if (windowSize === 'mobile') {
-    return <BlogSidebarMobile sidebar={sidebar} />;
-  }
-  return <BlogSidebarDesktop sidebar={sidebar} />;
+export default function BlogSidebar({ sidebar }: Props): ReactNode {
+    const windowSize = useWindowSize();
+    if (!sidebar?.items.length) {
+        return null;
+    }
+    // Mobile sidebar doesn't need to be server-rendered
+    if (windowSize === 'mobile') {
+        return <BlogSidebarMobile sidebar={sidebar} />;
+    }
+    return <BlogSidebarDesktop sidebar={sidebar} />;
 }
