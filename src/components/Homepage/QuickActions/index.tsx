b3b170e28759c4c0c6fa3bb6e15e43c17e55170c
Jannes <jannes@lets-book.com>
2025-10-13 12:58:33 +0200

    graphics

--- src/components/Homepage/QuickActions/index.tsx
+++ src/components/Homepage/QuickActions/index.tsx
@@ -49,7 +49,8 @@ const SettingsIcon = () => (
         strokeLinecap="round"
         strokeLinejoin="round"
     >
-        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+        <path d="M12 20h9"></path>
+        <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
     </svg>
 );
 
