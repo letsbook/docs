b95f206d5ba65a76932af913ab132ddea2e849b5
Laurent van Dommelen <laurent@lets-book.com>
2025-10-30 15:29:50 +0100

    Simplify the video player

---
+++ src/components/InlineVideoPlayer/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+type Props = {
+    videoSrc: string;
+    title?: string;
+};
+
+export default function InlineVideoPlayer({ videoSrc, title }: Props) {
+    return (
+        <video
+            playsInline
+            muted
+            width="100%"
+            className={styles.Root}
+            title={title}
+            autoPlay
+        >
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+    );
+}
